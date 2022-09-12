f905aa26cd66a16c161f222f32c7a993e155e64e
bitterkofte <hasantalhahtc@gmail.com>
2022-09-12 08:43:42 +0300

    JS file created

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const name = "mehmet";
+var lastname = "yılmaz";
+var fullname = name + lastname;
+
+var isAdult = age!== "18";
+var thisYear = new Date().getFullYear();
+
+console.log(!true);
+console.log("SUP!");
+console.log(name, lastname);
+console.log(fullname, age);
+var age = 20
+console.log("Birh Date:", thisYear - age);
